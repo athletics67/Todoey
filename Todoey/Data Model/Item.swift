0c0798dadc99d55703988729066eefaa778d2dfc
Craig Calvert <craigcalvert@icloud.com>
2018-01-09 17:07:10 -0800

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Craig Calvert on 1/9/18.
+//  Copyright © 2018 Craig Calvert. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
